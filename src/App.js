c24cd7d2fabb83db5fab11781d6e29b4e4bc154f
Iqra2124 <you@example.com>
2020-04-29 18:05:44 -0500

    Updated files

--- src/App.js
+++ src/App.js
@@ -8,10 +8,16 @@ import { Jumbotron } from "./components/Jumbotron";
 import { Auth } from "aws-amplify";
 
 function App(props) {
+  /* useState is a react hook with the format [variable, function]
+  ** Whatever is the arguement in the useState function is what the var is set to
+  ** Whenever the function like userHasAuth is called, it sets the variable isAuth to whatever is passed to it
+  ** React then reloads the whole page. This is a way of managing state variables
+  */
   const [isAuth, userHasAuth] = useState(false);
   const [isAuthenticating, setIsAuthenticating] = useState(true);
+  const [userCreds, setUserCreds] = useState(null);
 
-  // triggers onLoad the first time app is loaded
+  // triggers onLoad the first time app is loaded (empty list of var as param)
   useEffect(() => {
     onLoad();
   }, []);
@@ -21,6 +27,8 @@ function App(props) {
     try {
       await Auth.currentSession();
       userHasAuth(true);
+      const userCreds = await Auth.currentUserInfo();
+      setUserCreds(userCreds);
     }
     catch(e) {
       if (e !== 'No current user') {
@@ -35,37 +43,72 @@ function App(props) {
   async function handleLogout() {
     await Auth.signOut();
     userHasAuth(false);
+    setUserCreds(null);
     props.history.push("/login");
   }
 
   return (
     !isAuthenticating &&
-    <div className="App container">
-      <Navbar fluid collapseOnSelect>
-        <Navbar.Header>
-          <Navbar.Brand>
-            <Link to="/">Ramblerfy</Link>
-          </Navbar.Brand>
-        </Navbar.Header>
-        <Nav>
-          {isAuth
-            ? <NavItem onClick={handleLogout}>Logout</NavItem>
-            : <>
-                <LinkContainer to="/signup">
-                  <NavItem>Signup</NavItem>
-                </LinkContainer>
-                <LinkContainer to="/login">
-                  <NavItem>Login</NavItem>
-                </LinkContainer>
-              </>
-          }
-        </Nav>
-      </Navbar>
-      <Jumbotron />
-      { /*this handles all components rendered under the navbar/jumbo */ }
-      <Routes appProps={{ isAuth, userHasAuth }} />
+    <div className="App-container">
+      <header>
+        <nav>
+          <div className="row clearfix">
+            <LinkContainer to="/">
+              <img src={require("./images/android-chrome-512x512.png")} className="logo" />
+            </LinkContainer>
+            {isAuth
+              ? <ul
+                className="main-nav animated slideInRight"
+                id="check-status"
+                >
+                  <NavItem onClick={handleLogout}>Logout</NavItem>
+                  <LinkContainer to="/settings">
+                    <li><a href="#">SETTINGS</a></li>
+                  </LinkContainer>
+                  <LinkContainer to="/documentation">
+                    <li><a href="#">DOCUMENTATION</a></li>
+                  </LinkContainer>
+                  <LinkContainer to="/contact">
+                    <li><a href="#">CONTACT US</a></li>
+                  </LinkContainer>
+                </ul>
+
+              : <>
+                  <ul className="main-nav animated slideInRight" id="check-status">
+                    <LinkContainer to="/signup">
+                      <li><a href="#">SIGN UP</a></li>
+                    </LinkContainer>
+                    <LinkContainer to="/login">
+                      <li><a href="#">LOGIN</a></li>
+                    </LinkContainer>
+                    <LinkContainer to="/documentation">
+                      <li><a href="#">DOCUMENTATION</a></li>
+                    </LinkContainer>
+                    <LinkContainer to="/contact">
+                      <li><a href="#">CONTACT US</a></li>
+                    </LinkContainer>
+                  </ul>
+                </>
+            }
+            <a href="#" className="movable-icon" onclick="slideshow()"> <i className="fa fa-align-justify" /> </a>
+          </div>
+        </nav>
+        { /*this handles all components rendered under the navbar */ }
+        <Routes appProps={{ isAuth, userHasAuth, userCreds, setUserCreds }} />
+      </header>
     </div>
   );
 }
+
+/* driver connection example
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://<username>:<password>@ramblerpy-5rd9x.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+*/
 
 export default withRouter(App);
---
+++ src/containers/Contact.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import "./Contact.css";
+
+export default function Contact(props) {
+
+  function contactPage() {
+    return (
+      <div className="wrapper">
+        
+        <div className="information">
+          <div>
+          <h1>Contact Us</h1>
+          <h3>Contact us using the information below! <br /> Please fill out the form to the right :) </h3>
+            <info className="fa fa-map-marker" /><span className="details"> Loyola University Chicago </span><br />
+            <info className="fa fa-phone fa-lg" /><span className="details"> 1234567890 </span><br />
+            <info className="fa fa-envelope fa-lg" /><span className="details"> hello@gmail.com </span>
+          </div>
+          <form>
+            <div className="form" id="form1">
+              <h4>The fields with * are required.</h4>
+              <input type="text" className="input" placeholder="First & Last Name *" required /><br />
+              <input type="Email" className="input" placeholder="Email Address *" required /><br />
+              <input type="text" className="input" placeholder="Phone Number" /><br />
+              <textarea name="message" placeholder="Message *" rows={5} required defaultValue={""} /><br />
+              <button className="submit">Send Message</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="Contact">
+      {contactPage()}
+    </div>
+  );
+
+}
